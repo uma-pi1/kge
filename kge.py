992f49ab6bbeac464822e1212ff52082199df7dc
Rainer Gemulla <rgemulla@uni-mannheim.de>
2019-03-21 17:34:16 +0100

    Fixed indentation offset for kge.py

--- kge.py
+++ kge.py
@@ -6,48 +6,46 @@ from kge import job
 from kge.config import Config
 
 if __name__ == '__main__':
-  # default config
-  config = Config()
+    # default config
+    config = Config()
 
-  # parse arguments
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--config', type=str)
-  # TODO add all entries of config as arguments to parser
-  args = parser.parse_args()
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str)
+    # TODO add all entries of config as arguments to parser
+    args = parser.parse_args()
 
-  # load user config file (overwrites defaults)
-  if args.config is not None:
-    config.load(args.config)
+    # load user config file (overwrites defaults)
+    if args.config is not None:
+        config.load(args.config)
 
-  # TODO override with command line arguments
+    # validate arguments and set defaults
+    if config.folder() == '':
+        config.raw['output']['folder'] = \
+            'local/experiments/' \
+            + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") \
+            + "-" + config.raw['dataset']['name'] \
+            + "-" + config.raw['model']['name']
 
-  # validate arguments and set defaults
-  if config.folder() == '':
-    config.raw['output']['folder'] = \
-      'local/experiments/' \
-      + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") \
-      + "-" + config.raw['dataset']['name'] \
-      + "-" + config.raw['model']['name']
+    # create output folder
+    if os.path.exists(config.folder()):
+        # TODO
+        raise NotImplementedError("resume")
+    else:
+        os.makedirs(config.folder())
 
-  # create output folder
-  if os.path.exists(config.folder()):
-    # TODO
-    raise NotImplementedError("resume")
-  else:
-    os.makedirs(config.folder())
+    # store configuration in output folder
+    config.dump(config.folder() + "/config.yaml")
 
-  # store configuration in output folder
-  config.dump(config.folder() + "/config.yaml")
+    # print status information
+    config.log( yaml.dump(config.raw) )
 
-  # print status information
-  config.log( yaml.dump(config.raw) )
+    # create job
+    # if config['job']['type'] == 'grid':
+    #   job.grid_search_job.GridSearchExperiment(config)
+    # elif config['job']['type'] == 'bayesian':
+    #   job.bayesian_optimization_job.BayesianOptimizationExperiment(config)
+    # else:
+    #   raise ValueError('Unknown experiment type')
 
-  # create job
-  # if config['job']['type'] == 'grid':
-  #   job.grid_search_job.GridSearchExperiment(config)
-  # elif config['job']['type'] == 'bayesian':
-  #   job.bayesian_optimization_job.BayesianOptimizationExperiment(config)
-  # else:
-  #   raise ValueError('Unknown experiment type')
-
-  # Run/evaluate job
+    # Run/evaluate job
