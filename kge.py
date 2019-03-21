5fe714d276d13fe58d784dd84ecdc374a7cd8e75
Rainer Gemulla <rgemulla@uni-mannheim.de>
2019-03-21 16:52:40 +0100

    Load default config

--- kge.py
+++ kge.py
@@ -1,30 +1,44 @@
+import datetime
 import yaml
 import argparse
 from kge import job
 
 if __name__ == '__main__':
+  # load default config file
+  with open('kge/default.yaml', 'r') as file:
+    config = yaml.load(file)
 
-  # args parsing
+  # parse arguments
   parser = argparse.ArgumentParser()
-  parser.add_argument('--config_file', type=str)
+  parser.add_argument('--config', type=str)
+  # TODO add all entries of config as arguments to parser
   args = parser.parse_args()
 
-  # TODO: parse other arugments to override default settings
-  # - use argument of form "--key=value", where key is the name of a setting
+  # load user config file (overwrites defaults)
+  if args.config is not None:
+    with open(args.config, 'r') as file:
+      user_config = yaml.load(file)
+      # TODO deep merge into configs
 
-  # load settings from config file
-  with open(args.config_file, 'r') as file:
-    config = yaml.load(file)
+  # TODO override with command line arguments
+
+  # validate arguments and set defaults
+  if config['output']['folder'] == '':
+    config['output']['folder'] = \
+      datetime.datetime.now().strftime("%Y%m%d-%H%M") \
+      + "-" + config['dataset']['name'] \
+      + "-" + config['model']['name']
+
+  # print status information
+  # TODO nicer
   print(yaml.dump(config))
 
-  # TODO: override settings with command line args
-
   # create job
-  if config['job']['type'] == 'grid':
-    job.grid_search_job.GridSearchExperiment(config)
-  elif config['job']['type'] == 'bayesian':
-    job.bayesian_optimization_job.BayesianOptimizationExperiment(config)
-  else:
-    raise ValueError('Unknown experiment type')
+  # if config['job']['type'] == 'grid':
+  #   job.grid_search_job.GridSearchExperiment(config)
+  # elif config['job']['type'] == 'bayesian':
+  #   job.bayesian_optimization_job.BayesianOptimizationExperiment(config)
+  # else:
+  #   raise ValueError('Unknown experiment type')
 
   # Run/evaluate job
