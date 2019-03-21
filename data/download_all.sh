be6cf18adea95441bc3e0b995c72a63fe92b3a99
Rainer Gemulla <rgemulla@uni-mannheim.de>
2019-03-21 15:46:39 +0100

    Updated dataset download script

--- data/download_all.sh
+++ data/download_all.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-mkdir data
+BASEDIR=`pwd`
+
+if [ ! -d "$BASEDIR/data" ]; then
+    mkdir "$BASEDIR/data"
+fi
 
 # FB15K
-if [ ! -d "data/FB15k" ]; then
+if [ ! -d "$BASEDIR/data/FB15k" ]; then
   echo Downloading FB15k
-  cd data
+  cd $BASEDIR/data
   curl -O https://www.wim.uni-mannheim.de/fileadmin/lehrstuehle/pi1/pi1/BK-EMB/FB15k.zip
   # http://web.informatik.uni-mannheim.de/RuleN/data/FB15k.zip
   unzip FB15k.zip
@@ -22,29 +26,26 @@ if [ ! -d "data/FB15k" ]; then
           ln -s freebase_mtr100_mte100-test.txt test.txt
           ;;
   esac
-  cd ../..
-  python preprocess.py FB15k
 else
     echo FB15k already present
 fi
 
 # FB15K-237
-if [ ! -d "data/FB15k-237" ]; then
+if [ ! -d "$BASEDIR/data/FB15k-237" ]; then
   echo Downloading FB15k-237
-  cd data
+  cd $BASEDIR/data
   curl -O https://www.wim.uni-mannheim.de/fileadmin/lehrstuehle/pi1/pi1/BK-EMB/FB15k-237.zip
   # http://web.informatik.uni-mannheim.de/RuleN/data/FB15k-237.zip
   unzip FB15k-237.zip
   cd ..
-  python preprocess.py FB15k-237
 else
     echo FB15k-237 already present
 fi
 
 # WN18
-if [ ! -d "data/WN18" ]; then
+if [ ! -d "$BASEDIR/data/WN18" ]; then
   echo Downloading WN18
-  cd data
+  cd $BASEDIR/data
   curl -O https://www.wim.uni-mannheim.de/fileadmin/lehrstuehle/pi1/pi1/BK-EMB/WN18.zip
   # http://web.informatik.uni-mannheim.de/RuleN/data/WN18.zip
   unzip WN18.zip
@@ -61,53 +62,45 @@ if [ ! -d "data/WN18" ]; then
           ln -s wordnet-mlj12-test.txt test.txt
           ;;
   esac
-  cd ../..
-  python preprocess.py WN18
 else
     echo WN18 already present
 fi
 
 # WN18RR
-if [ ! -d "data/WN18RR" ]; then
+if [ ! -d "$BASEDIR/data/WN18RR" ]; then
   echo Downloading WN18RR
-  cd data
+  cd $BASEDIR/data
   curl -O https://www.wim.uni-mannheim.de/fileadmin/lehrstuehle/pi1/pi1/BK-EMB/WN18RR.tar.gz
   tar xvf WN18RR.tar.gz
-  cd ..
-  python preprocess.py WN18RR
 else
     echo WN18RR already present
 fi
 
 
 # DBpedia50
-if [ ! -d "data/dbpedia50" ]; then
+if [ ! -d "$BASEDIR/data/dbpedia50" ]; then
   echo Downloading dbpedia50
-  cd data
+  cd $BASEDIR/data
   curl -O https://www.wim.uni-mannheim.de/fileadmin/lehrstuehle/pi1/pi1/BK-EMB/dbpedia50.tar.gz 
   tar xvf dbpedia50.tar.gz
-  cd ..
-  python preprocess.py dbpedia50
 else
     echo dbpedia50 already present
 fi
 
 # DBpedia500
-if [ ! -d "data/dbpedia500" ]; then
+if [ ! -d "$BASEDIR/data/dbpedia500" ]; then
   echo Downloading dbpedia500
-  cd data
+  cd $BASEDIR/data
   curl -O https://www.wim.uni-mannheim.de/fileadmin/lehrstuehle/pi1/pi1/BK-EMB/dbpedia500.tar.gz 
   tar xvf dbpedia500.tar.gz
-  cd ..
-  python preprocess.py dbpedia500
 else
     echo dbpedia500 already present
 fi
 
 # DBpedia100
-if [ ! -d "data/DB100K" ]; then
+if [ ! -d "$BASEDIR/data/DB100K" ]; then
   echo Downloading DB100K
-  cd data
+  cd $BASEDIR/data
   curl -O https://www.wim.uni-mannheim.de/fileadmin/lehrstuehle/pi1/pi1/BK-EMB/DB100K.tar.gz 
   tar xvf DB100K.tar.gz
   cd DB100K
@@ -123,20 +116,15 @@ if [ ! -d "data/DB100K" ]; then
           ln -s _test.txt test.txt
           ;;
   esac
-  cd ../..
-  python preprocess.py DB100K
 else
     echo DB100K already present
 fi
 
 # YAGO3-10
-if [ ! -d "data/YAGO3-10" ]; then
+if [ ! -d "$BASEDIR/data/YAGO3-10" ]; then
   echo Downloading YAGO3-10
-  cd data
-  curl -O https://www.wim.uni-mannheim.de/fileadmin/lehrstuehle/pi1/pi1/BK-EMB/YAGO3-10.tar.gz 
+  curl -O https://www.wim.uni-mannheim.de/fileadmin/lehrstuehle/pi1/pi1/BK-EMB/YAGO3-10.tar.gz
   tar xvf YAGO3-10.tar.gz
-  cd ..
-  python preprocess.py YAGO3-10
 else
     echo YAGO3-10 already present
 fi
