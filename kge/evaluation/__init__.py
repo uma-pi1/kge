1ee6b81127b62b00c49f15affe21f944a4f3baa3
Daniel Ruffinelli <daniel.ruffinelli@gmail.com>
2019-03-29 18:23:32 +0100

    Reorganizing files

--- kge/evaluation/__init__.py
+++
@@ -1,3 +0,0 @@
-from kge.evaluation.evaluation_job import EvaluationJob
-from kge.evaluation.entity_ranking import EntityRanking
-from kge.evaluation.entity_pair_ranking import EntityPairRanking
--- kge/evaluation/entity_pair_ranking.py
+++
@@ -1,8 +0,0 @@
-from kge.evaluation import EvaluationJob
-
-
-class EntityPairRanking(EvaluationJob):
-    """ Entity-pair ranking evaluation protocol """
-
-    def __init__(self, config, dataset, model):
-        super().__init__(config, dataset, model)
