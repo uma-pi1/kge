eab39e89b76e22d12f5b4edbd977ed011ef0b0e5
Samuel Broscheit <samuel.broscheit@gmail.com>
2019-04-04 23:59:13 +0200

    Added DistMult

--- kge/model/distmult.py
+++ kge/model/distmult.py
@@ -1,7 +1,39 @@
-from kge.model.base_model import BaseModel
+from kge.model.kge_model import KgeModel, KgeEmbedder
 
 
-class DistMult(BaseModel):
+class DistMult(KgeModel):
     """
-    Implements the 1:1, 1:N, N:N forward functions
+    DistMult
     """
+
+    def __init__(self, config, dataset):
+        super().__init__(config, dataset)
+        self.entity_embedder = KgeEmbedder.create(config, dataset, True)
+        self.relation_embedder = KgeEmbedder.create(config, dataset, False)
+
+
+    def _score(self, s, p, o, prefix=None):
+        r"""
+        :param s: tensor of size [batch_size, embedding_size]
+        :param p: tensor of size [batch_size, embedding_size]
+        :param o:: tensor of size [batch_size, embedding_size]
+        :return: score tensor of size [batch_size, 1]
+        """
+        sub = s.view(-1, s.size(-1))
+        rel = p.view(-1, p.size(-1))
+        obj = o.view(-1, o.size(-1))
+
+        batch_size = p.size(0)
+        feat_dim = 1
+
+        if prefix:
+          if prefix == 'sp':
+              out = (sub * rel).mm(obj.transpose(0, 1))
+          elif prefix == 'po':
+              out = (rel * obj).mm(sub.transpose(0, 1))
+          else:
+              raise Exception
+        else:
+            out = (sub * obj * rel).sum(dim=feat_dim)
+
+        return out.view(batch_size, -1)
