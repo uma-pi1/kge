6efc9d77100f006abb59b39ba1308a90ef43253a
Samuel Broscheit <samuel.broscheit@gmail.com>
2019-04-05 02:08:34 +0200

    Put loss into its own file (more obvious to find)

--- kge/util/misc.py
+++ kge/util/misc.py
@@ -1,21 +1,4 @@
 import subprocess
-import torch
-
-
-class KgeLoss:
-    """ Wraps torch loss functions """
-
-    def create(config):
-        """ Factory method for loss creation """
-        if config.get('train.loss') == 'ce':
-            return torch.nn.CrossEntropyLoss(reduction='mean')
-        elif config.get('train.loss') == 'bce':
-            return torch.nn.BCEWithLogitsLoss(reduction='mean')
-        elif config.get('train.loss') == 'kl':
-            return torch.nn.KLDivLoss(reduction='mean')
-        else:
-            # perhaps TODO: try class with specified name -> extensibility
-            raise ValueError('train.loss')
 
 def is_number(s, number_type):
     """ Returns True is string is a number. """
