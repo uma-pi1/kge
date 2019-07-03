9e345ab3c114a49272bbcfd9a3520dbd40e633a0
Daniel Ruffinelli <daniel.ruffinelli@gmail.com>
2019-07-03 16:15:54 +0200

    TransE is now more memory efficient.

--- kge/model/transe.py
+++ kge/model/transe.py
@@ -10,12 +10,28 @@ class TransEScorer(RelationalScorer):
         super().__init__(config, dataset)
         self._norm = config.get("transe.l_norm")
 
-    def score_emb_spo(self, s_emb, p_emb, o_emb):
-        n = p_emb.size(0)
-        out = torch.norm(s_emb + p_emb - o_emb,
-                         p=self._norm,
-                         dim=1) * -1
-        return out.view(n, -1)
+    def score_emb(self, s_emb, p_emb, o_emb, combine: str):
+            n = p_emb.size(0)
+            if combine == "spo":
+                out = torch.norm(s_emb + p_emb - o_emb,
+                                 p=self._norm,
+                                 dim=1) * -1
+            elif combine == "sp*":
+                out = torch.zeros(n, o_emb.size(0)).to(self.config.get("job.device"))
+                for i in range(n):
+                    out[i,:] = torch.norm((s_emb[i,:] + p_emb[i,:]) - o_emb,
+                                          p=self._norm,
+                                          dim=1) * -1
+            elif combine == "*po":
+                out = torch.zeros(n, s_emb.size(0)).to(self.config.get("job.device"))
+                for i in range(n):
+                    out[i,:] = torch.norm(s_emb + (p_emb[i,:] - o_emb[i,:]),
+                                          p=self._norm,
+                                          dim=1) * -1
+            else:
+                raise ValueError('cannot handle combine="{}".format(combine)')
+
+            return out.view(n, -1)
 
 
 class TransE(KgeModel):
