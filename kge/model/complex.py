de96edffda90295472345a002088540021e414e0
Rainer Gemulla <rgemulla@uni-mannheim.de>
2019-07-05 12:09:28 +0200

    Added auto-init to complex

--- kge/model/complex.py
+++ kge/model/complex.py
@@ -1,10 +1,17 @@
+import math
 import torch
 from kge import Config, Dataset
 from kge.model.kge_model import RelationalScorer, KgeModel
 
 
 class ComplExScorer(RelationalScorer):
-    r"""Implementation of the ComplEx KGE scorer."""
+    r"""Implementation of the ComplEx KGE scorer.
+
+    Reference: Théo Trouillon, Johannes Welbl, Sebastian Riedel, Éric Gaussier and
+    Guillaume Bouchard: Complex Embeddings for Simple Link Prediction. ICML 2016.
+    `<http://proceedings.mlr.press/v48/trouillon16.pdf>`_
+
+    """
 
     def __init__(self, config: Config, dataset: Dataset):
         super().__init__(config, dataset)
@@ -13,17 +20,18 @@ class ComplExScorer(RelationalScorer):
         n = p_emb.size(0)
 
         # Here we use a fast implementation of computing the ComplEx scores using
-        # Hadamard products. TODO add details / reference to paper
+        # Hadamard products, as in Eq. (11) of paper.
+        #
+        # Split the relation and object embeddings into real part (first half) and
+        # imaginary part (second half).
+        p_emb_re, p_emb_im = (t.contiguous() for t in p_emb.chunk(2, dim=1))
+        o_emb_re, o_emb_im = (t.contiguous() for t in o_emb.chunk(2, dim=1))
 
-        # split the relation and object embeddings into two halves
-        p_emb_left, p_emb_right = (t.contiguous() for t in p_emb.chunk(2, dim=1))
-        o_emb_left, o_emb_right = (t.contiguous() for t in o_emb.chunk(2, dim=1))
+        # combine them again to create a column block for each required combination
+        s_all = torch.cat((s_emb, s_emb), dim=1)  # re, im, re, im
+        r_all = torch.cat((p_emb_re, p_emb, -p_emb_im), dim=1)  # re, re, im, -im
+        o_all = torch.cat((o_emb, o_emb_im, o_emb_re), dim=1)  # re, im, im, re
 
-        # and combine them again
-        s_all = torch.cat((s_emb, s_emb), dim=1)
-        r_all = torch.cat((p_emb_left, p_emb, -p_emb_right), dim=1)
-
-        o_all = torch.cat((o_emb, o_emb_right, o_emb_left), dim=1)
         if combine == "spo":
             out = (s_all * o_all * r_all).sum(dim=1)
         elif combine == "sp*":
@@ -40,7 +48,40 @@ class ComplEx(KgeModel):
     r"""Implementation of the ComplEx KGE model."""
 
     def __init__(self, config: Config, dataset: Dataset, configuration_key=None):
-        super().__init__(config,
-                         dataset,
-                         ComplExScorer(config, dataset),
-                         configuration_key=configuration_key)
+        # auto initialize such that scores have unit variance
+        if config.get(config.get("model") + ".auto_initialization"):
+            # Var[score] = 4*(dim/2)*Var[e]^2*Var[r], where e/r are the entries in the
+            # embeddings and (dim/2) is the embedding size in complex space
+            #
+            # Thus we set Var[e]=Var[r]=(1.0/(2.0*dim))^(1/3)
+            dim = config.get_first(
+                config.get("model") + ".entity_embedder.dim",
+                config.get(config.get("model") + ".entity_embedder.type") + ".dim",
+            )
+            s = math.pow(1.0 / 2.0 / dim, 1.0 / 6.0)
+
+            config.set(
+                config.get("model") + ".entity_embedder.initialize", "normal_", log=True
+            )
+            config.set(
+                config.get("model") + ".entity_embedder.initialize_args",
+                {"mean": 0.0, "std": s},
+                log=True,
+            )
+            config.set(
+                config.get("model") + ".relation_embedder.initialize",
+                "normal_",
+                log=True,
+            )
+            config.set(
+                config.get("model") + ".relation_embedder.initialize_args",
+                {"mean": 0.0, "std": s},
+                log=True,
+            )
+
+        super().__init__(
+            config,
+            dataset,
+            ComplExScorer(config, dataset),
+            configuration_key=configuration_key,
+        )
