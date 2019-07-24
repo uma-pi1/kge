cb8da72a83fd72cad4f8cdb780047c68f3bcec22
Felix Kleinöder <felix.kleinoeder@web.de>
2019-07-24 12:21:05 +0200

    -Run method in auto-search is now abstract -Old run method in auto-search moved to ax_search -Quick fix ax_search.run() such that after 5 trials register_trial uses parameters, because before this method crashed when calling it after 5 trials. Maybe the fixed value of 5 must be adapted. I don't know where ax takes this value from.
    Signed-off-by: Felix Kleinöder <felix.kleinoeder@web.de>

--- kge/job/ax_search.py
+++ kge/job/ax_search.py
@@ -1,5 +1,7 @@
+import concurrent.futures
 from kge.job import AutoSearchJob
 from kge import Config
+import kge.job.search
 from ax.service.ax_client import AxClient
 from typing import List
 
@@ -65,3 +67,113 @@ class AxSearchJob(AutoSearchJob):
     def get_best_parameters(self):
         best_parameters, values = self.ax_client.get_best_parameters()
         return best_parameters, float(values[0]["metric_value"])
+
+    def run(self):
+        self.init_search()
+
+        # let's go
+        trial_no = 0
+        num_trials = self.config.get("ax_search.num_trials")
+        while trial_no < num_trials:
+            self.config.log(
+                "Registering trial {}/{}...".format(trial_no, num_trials - 1)
+            )
+
+            # determine next trial
+            if trial_no >= len(self.parameters):
+                # create a new trial
+                if trial_no >= 5:
+                    parameters, trial_id = self.register_trial(parameters)
+                else:
+                    parameters, trial_id = self.register_trial()
+                if trial_id is None:
+                    self.config.log(
+                        "Cannot generate trial parameters. Will try again after a "
+                        + "running trial has completed."
+                    )
+                else:
+                    # remember the trial
+                    self.trial_ids.append(trial_id)
+                    self.parameters.append(parameters)
+                    self.results.append(None)
+            else:
+                # use the trial of a resumed run of this job
+                parameters, trial_id = self.register_trial(self.parameters[trial_no])
+                self.trial_ids.append(trial_id)
+
+            # create job for trial
+            if trial_id is not None:
+                folder = str("{:05d}".format(trial_no))
+                config = self.config.clone(folder)
+                config.set("job.type", "train")
+                config.set_all(parameters)
+                config.init_folder()
+
+            # run or schedule the trial
+            if trial_id is not None:
+                self.submit_task(
+                    kge.job.search._run_train_job,
+                    (
+                        self,
+                        trial_no,
+                        config,
+                        self.config.get("ax_search.num_trials"),
+                        list(parameters.keys()),
+                    ),
+                )
+
+                # on last iteration, wait for all running trials to complete
+                if trial_no == num_trials - 1:
+                    self.wait_task(return_when=concurrent.futures.ALL_COMPLETED)
+            else:
+                # couldn't generate a new trial since data is lacking; so wait
+                self.wait_task()
+
+            # for each ready trial, store its results
+            for ready_trial_no, ready_trial_best, _ in self.ready_task_results:
+                self.config.log(
+                    "Registering trial {} result: {}".format(
+                        ready_trial_no, ready_trial_best["metric_value"]
+                    )
+                )
+                self.results[ready_trial_no] = ready_trial_best
+                self.register_trial_result(
+                    self.trial_ids[ready_trial_no],
+                    self.parameters[ready_trial_no],
+                    ready_trial_best,
+                )
+
+                # save checkpoint
+                # TODO make atomic (may corrupt good checkpoint when canceled!)
+                self.save(self.config.checkpoint_file(1))
+
+            # clean up
+            self.ready_task_results.clear()
+            if trial_id is not None:
+                # advance to next trial (unless we did not run this one)
+                trial_no += 1
+
+        # all done, output best trial result
+        trial_metric_values = list(
+            map(lambda trial_best: trial_best["metric_value"], self.results)
+        )
+        best_trial_index = trial_metric_values.index(max(trial_metric_values))
+        metric_name = self.results[best_trial_index]["metric_name"]
+        self.config.log(
+            "Best trial: {}={}".format(
+                metric_name, trial_metric_values[best_trial_index]
+            )
+        )
+        self.trace(echo=True,
+                   echo_prefix="  ",
+                   log=True,
+                   scope="search",
+                   **self.results[best_trial_index])
+
+        # DISABLED FOR NOW SINCE IDENTICAL TO BEST TRIAL
+        # output parameter estimates
+        # best_parameters, best_value_estimate = self.get_best_parameters()
+        # self.config.log(
+        #     "Search result (estimate): {}={}".format(metric_name, best_value_estimate)
+        # )
+        # self.config.log("parameters: {}".format(best_parameters), prefix="  ")
--- kge/job/hyperband_search.py
+++ kge/job/hyperband_search.py
@@ -9,13 +9,17 @@ class HyperbandSearchJob(AutoSearchJob):
         super().__init__(config, dataset, parent_job)
 
     def init_search(self):
+        pass
 
 
     def register_trial(self, parameters=None):
+        pass
 
 
     def register_trial_result(self, trial_id, parameters, trace_entry):
+        pass
 
 
     def get_best_parameters(self):
+        pass
 
