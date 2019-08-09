99db768244bae9392735e97edaae4529ab6d3f30
samuelbroscheit <samuel.broscheit@gmail.com>
2019-08-09 19:41:52 +0200

    make git only work when git binary is found

--- kge/util/misc.py
+++ kge/util/misc.py
@@ -17,19 +17,41 @@ def is_number(s, number_type):
 
 # from https://stackoverflow.com/questions/14989858/get-the-current-git-hash-in-a-python-script
 def get_git_revision_hash():
-    with Path(kge_base_dir()):
-        return subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode()
-
+    if which('git') is not None:
+        with Path(kge_base_dir()):
+            return subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode()
+    else:
+        return 'no git binary found'
 
 # from https://stackoverflow.com/questions/14989858/get-the-current-git-hash-in-a-python-script
 def get_git_revision_short_hash():
-    with Path(kge_base_dir()):
-        return (
-            subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
-            .strip()
-            .decode()
-        )
+    if which('git') is not None:
+        with Path(kge_base_dir()):
+            return (
+                subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+                .strip()
+                .decode()
+            )
+    else:
+        return 'no git binary found'
 
+# from https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
 
 def kge_base_dir():
     return os.path.abspath(filename_in_module(kge, ".."))
