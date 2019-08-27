f5621cf123a8c9ea7f5da6e891c2688949cecb88
Rainer Gemulla <rgemulla@uni-mannheim.de>
2019-08-27 18:18:09 +0200

    Temporary fix for cdist bug of pytorch (now really?)
    https://github.com/pytorch/pytorch/issues/22353

--- kge/model/transe.py
+++ kge/model/transe.py
@@ -11,13 +11,19 @@ class TransEScorer(RelationalScorer):
         self._norm = self.get_option("l_norm")
 
     def score_emb(self, s_emb, p_emb, o_emb, combine: str):
+        # TODO drop [None,:,:] everywhere once the cdist bug (
+        # https://github.com/pytorch/pytorch/issues/22353) has been fixed. The fix is
+        # https://github.com/pytorch/pytorch/commit/c33adf539c82fe26ed678a2aea4427fbbcbd7c97,
+        # but has not been released (released in pytorch 1.2.1?).
         n = p_emb.size(0)
         if combine == "spo":
             out = -F.pairwise_distance(s_emb + p_emb, o_emb, p=self._norm)
         elif combine == "sp*":
-            out = -torch.cdist(s_emb + p_emb, o_emb, p=self._norm)
+            sp_emb = s_emb + p_emb
+            out = -torch.cdist(sp_emb[None,:,:], o_emb[None,:,:], p=self._norm)
         elif combine == "*po":
-            out = -torch.cdist(o_emb - p_emb, s_emb, p=self._norm)
+            po_emb = o_emb - p_emb
+            out = -torch.cdist(po_emb[None,:,:], s_emb[None,:,:], p=self._norm)
         else:
             raise ValueError('cannot handle combine="{}".format(combine)')
         return out.view(n, -1)
