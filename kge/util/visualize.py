eb7249bb31bc9375fc0ae7ebe8fa59375f12bc7c
nzteb <patrickmbetz@gmail.com>
2019-09-15 12:28:26 +0200

    Visualizations: Generic hook appending

--- kge/util/visualize.py
+++ kge/util/visualize.py
@@ -2,6 +2,7 @@ from kge.job.trace import Trace
 from kge.job import Job,TrainingJob,SearchJob
 import yaml
 import visdom
+import re
 
 
 
@@ -12,12 +13,12 @@ class VisualizationHandler():
      :param tracking: "broadcast" or "post" (for post processing the tracefiles..)
      """
 
-    def __init__(self, writer, jobtype=None, tracking=None, include_train=None, include_eval=None):
+    def __init__(self, writer, tracking=None, include_train=None, include_eval=None, jobconfig=None):
         self.writer = writer
-        self.jobtype = type
         self.tracking = tracking
         self.include_train = include_train
         self.include_eval = include_eval
+        self.jobconfig = jobconfig
 
     def process_trace(self, tracefile, tracetype, jobtype):
         """ Takes a trace file and processes it.
@@ -36,27 +37,27 @@ class VisualizationHandler():
     def process_trace_entry(self, trace_entry, tracetype, jobtype):
         """ Process some trace entry.
 
-        Note there can be "job"=train like entries in a searchtype trace and there can be a traintype trace in a
-        search type job. Depending on this configuration and on "broadcast" vs "post" behavior is slightly different.
+       Note that there are different settings which can occur: E. g. a searchjob can have training traces which also have
+       eval entries. This has to be tracked because then depending on "broadcast" or "post" behavior differs in the
+       various settings.
 
         """
         entry_keys = list(trace_entry.keys())
         epoch = trace_entry.get("epoch")
         if epoch:
-            include_list = []
+            include_patterns = []
             if trace_entry["job"] == "train":
-                include_list = self.include_train
+                include_patterns = self.include_train
                 visualize = self._visualize_train_item
             elif trace_entry["job"] == "eval" and tracetype != "search":
-                include_list = self.include_eval
+                include_patterns = self.include_eval
                 visualize = self._visualize_eval_item
             elif trace_entry["job"] == "search":
                 self._process_search_trace_entry(trace_entry)
                 return
-            for key in entry_keys:
-                #TODO change for regex here
-                if key in include_list:
-                    visualize(key, trace_entry[key], epoch, tracetype, jobtype)
+            for pattern in include_patterns:
+                for matched_key in list(filter(lambda key: re.match(pattern, key), entry_keys)):
+                    visualize(matched_key, trace_entry[matched_key], epoch, tracetype, jobtype)
 
     def _process_search_trace_entry(self, trace_entry):
         raise NotImplementedError
@@ -67,29 +68,78 @@ class VisualizationHandler():
     def _visualize_eval_item(self, key, value, epoch, tracetype, jobtype):
         raise NotImplementedError
 
+    @classmethod
+    def register_broadcast(cls, **kwargs):
+        """ Bundles the different information that are needed to process broadcasting and registers hooks.
+
+        The user parameter inputs are collected and depending on the jobtype, broadcasting functionality is
+        registered as hooks.
+
+        """
+
+        def init_hooks(job):
+
+            if isinstance(job, TrainingJob):
+                # TODO for tensorboard this probably has to be changed to path or so
+                # TODO and then decision has to be made where to store the tensorboard event file
+                envname = str(job.config.folder).replace("_", "-")
+                envname = envname.split("/")
+                envname = envname[-1]
+
+                handler = VisualizationHandler.create(
+                    module=kwargs["module"],
+                    tracking=kwargs["tracking"],
+                    include_train=kwargs["include_train"],
+                    include_eval=kwargs["include_eval"],
+                    env=envname
+                )
+                tracetype = None
+                jobtype = None
+                # pure training job
+                if job.parent_job == None:
+                    tracetype = "train"
+                    jobtype = "train"
+               # some search job
+                if isinstance(job.parent_job, SearchJob):
+                    tracetype = "train"
+                    jobtype = "search"
+
+                def visualize_data(job, trace_entry):
+                    handler.process_trace_entry(trace_entry, tracetype=tracetype, jobtype=jobtype)
+                job.post_epoch_hooks.append(visualize_data)
+                job.valid_job.post_valid_hooks.append(visualize_data)
+
+        Job.job_created_hooks.append(init_hooks)
+
+    @classmethod
+    def create(cls, **kwargs):
+        if kwargs["module"] == "visdom":
+            vis = visdom.Visdom(env=kwargs["env"])
+            return VisdomHandler(
+                vis,
+                tracking = kwargs["tracking"],
+                include_train=kwargs["include_train"],
+                include_eval=kwargs["include_eval"],
+                envname=kwargs["env"],
+            )
 
 class VisdomHandler(VisualizationHandler):
-    def __init__(self, writer, include_train, include_eval, main_envname=None, sub_envname=None, type=False, config=None):
-        super().__init__(writer, type, include_train=include_train, include_eval=include_eval)
-        self.main_envname = main_envname
-        self.sub_envname = sub_envname
+    def __init__(
+            self,
+            writer,
+            tracking=None,
+            include_train=None,
+            include_eval=None,
+            envname=None,
+            config=None,
+            jobconfig=None):
+
+        super().__init__(writer, tracking, include_train, include_eval, jobconfig)
+        self.envname = envname
         self.config = config
 
-    def _visualize_train_item(self, key, value, epoch, tracetype, jobtype):
-        if jobtype == "train":
-            self._visualize_item(key, value, epoch, tracetype, jobtype)
-
-    def _visualize_eval_item(self, key, value, epoch, tracetype, jobtype):
-        if jobtype == "train":
-            self._visualize_item(key, value, epoch, tracetype, jobtype)
-
     def _visualize_item(self, key, value, epoch, tracetype, jobtype):
-        env = None
-        if self.sub_envname:
-            env =  self.sub_envname
-        else:
-            env = self.main_envname
-
+        env = self.envname
         self.writer.env = env
         self.writer.line(
             X=[epoch],
@@ -99,6 +149,17 @@ class VisdomHandler(VisualizationHandler):
             name=env.split("_")[-1],
             update="append"
         )
+
+    def _visualize_train_item(self, key, value, epoch, tracetype, jobtype):
+        if jobtype == "train":
+            self._visualize_item(key, value, epoch, tracetype, jobtype)
+        if jobtype == "search":
+            self._visualize_item(key, value, epoch, tracetype, jobtype)
+
+
+    def _visualize_eval_item(self, key, value, epoch, tracetype, jobtype):
+        if jobtype == "train":
+            self._visualize_item(key, value, epoch, tracetype, jobtype)
 
     def _get_opts(self, title, **kwargs):
         opts = {
@@ -128,28 +189,15 @@ class VisdomHandler(VisualizationHandler):
             opts[key] = value
         return opts
 
+def initialize_visualization(vis_config):
+    if vis_config["tracking"] == "broadcast":
 
-
-
-
-def initialize(job):
-
-    if isinstance(job, TrainingJob) and job.parent_job == None:
-        envname = str(job.config.folder).replace("_", "-")
-        envname = envname.split("/")
-        train_envname = envname[-1]
-        vis = visdom.Visdom(env=train_envname)
-
-        include_train = ["avg_loss", "avg_cost", "avg_penalty", "forward_time", "epoch_time"]
-        include_eval = ["mean_rank_filtered"]
-        handler = VisdomHandler(vis, main_envname=train_envname, include_train=include_train, include_eval=include_eval)
-
-        def hook1(job, trace_entry):
-            handler.process_trace_entry(trace_entry, tracetype="train", jobtype="train")
-
-        job.post_epoch_hooks.append(hook1)
-        job.valid_job.post_valid_hooks.append(hook1)
-
+        VisualizationHandler.register_broadcast(
+            module=vis_config["module"],
+            include_train = ["avg_loss", "avg_cost", "avg_penalty", "forward_time", "epoch_time"],
+            include_eval = ["mean_rank_filtered"],
+            tracking = vis_config["tracking"]
+        )
 
 
 
