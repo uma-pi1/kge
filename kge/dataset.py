37b48f9aff15ab406228e516b4c3092574cb7a0c
Rainer Gemulla <rgemulla@uni-mannheim.de>
2019-12-20 16:52:19 +0100

    Added support for entity&relation strings

--- kge/dataset.py
+++ kge/dataset.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import sys
 
 import torch
 from torch import Tensor
@@ -89,6 +90,7 @@ class Dataset(Configurable):
         return torch.from_numpy(triples)
 
     def load_triples(self, key: str) -> Tensor:
+        "Load or return the triples with the specified key."
         if key not in self._triples:
             filename = self.config.get(f"dataset.files.{key}.filename")
             filetype = self.config.get(f"dataset.files.{key}.type")
@@ -105,28 +107,59 @@ class Dataset(Configurable):
 
     @staticmethod
     def _load_map(
-        filename: str, as_list: bool = False, delimiter: str = "\t"
+        filename: str,
+        as_list: bool = False,
+        delimiter: str = "\t",
+        ignore_duplicates=False,
     ) -> Union[List, Dict]:
         n = 0
         dictionary = {}
+        warned_overrides = False
+        duplicates = 0
         with open(filename, "r") as file:
             for line in file:
                 key, value = line.split(delimiter, maxsplit=1)
+                value = value.rstrip("\n")
                 if as_list:
                     key = int(key)
                     n = max(n, key + 1)
-                dictionary[key] = value
+                if key in dictionary:
+                    duplicates += 1
+                    if not ignore_duplicates:
+                        raise KeyError(f"{filename} contains duplicated keys")
+                else:
+                    dictionary[key] = value
         if as_list:
             array = [None] * n
             for index, value in dictionary.items():
                 array[index] = value
-            return array
+            return array, duplicates
         else:
-            return dictionary
+            return dictionary, duplicates
 
     def load_map(
-        self, key: str, as_list: bool = False, maptype=None
+        self,
+        key: str,
+        as_list: bool = False,
+        maptype=None,
+        ids_key=None,
+        ignore_duplicates=False,
     ) -> Union[List, Dict]:
+        """Load or return the map with the specified key.
+
+        If `as_list` is set, the map is converted to an array indexed by the map's keys.
+
+        If `maptype` is set ensures that the map being loaded has the specified type.
+        Valid map types are `map` (keys are indexes) and `idmap` (keys are ids).
+
+        If the map is of type `idmap`, its keys can be converted to indexes by setting
+        `ids_key` to either `entity_ids` or `relation_ids` and `as_list` to `True`.
+
+        If ignore_duplicates is set to `False` and the map contains duplicate keys,
+        raise a `KeyError`. Otherwise, logs a warning and picks first occurrence of a
+        key.
+
+        """
         if key not in self._meta:
             filename = self.config.get(f"dataset.files.{key}.filename")
             filetype = self.config.get(f"dataset.files.{key}.type")
@@ -139,9 +172,32 @@ class Dataset(Configurable):
                     "Unexpected file type: "
                     f"dataset.files.{key}.type='{filetype}', expected {maptype}"
                 )
-            map_ = Dataset._load_map(
-                os.path.join(self.folder, filename), as_list=as_list
-            )
+            if filetype == "idmap" and as_list and ids_key:
+                map_, duplicates = Dataset._load_map(
+                    os.path.join(self.folder, filename),
+                    as_list=False,
+                    ignore_duplicates=ignore_duplicates,
+                )
+                ids = self.load_map(ids_key, as_list=True)
+                map_ = [map_.get(ids[i], None) for i in range(len(ids))]
+                nones = map_.count(None)
+                if nones > 0:
+                    self.config.log(
+                        f"Warning: could not find {nones} ids in map {key}; "
+                        "filling with None."
+                    )
+            else:
+                map_, duplicates = Dataset._load_map(
+                    os.path.join(self.folder, filename),
+                    as_list=as_list,
+                    ignore_duplicates=ignore_duplicates,
+                )
+
+            if duplicates > 0:
+                self.config.log(
+                    f"Warning: map {key} contains {duplicates} duplicate keys, "
+                    "all which have been ignored"
+                )
             self._meta[key] = map_
 
         return self._meta[key]
@@ -160,7 +216,6 @@ class Dataset(Configurable):
         copy._meta = self._meta
         copy._indexes = self._indexes
         return copy
-
 
     ## ACCESS ###########################################################################
 
@@ -186,34 +241,78 @@ class Dataset(Configurable):
         return self.load_triples(split)
 
     def train(self) -> Tensor:
-        "Return training split."
+        """Return training split.
+
+        If the split is not yet loaded, load it. Returns an Nx3 IntTensor of
+        spo-triples.
+
+        """
         return self.split("train")
 
     def valid(self) -> Tensor:
-        "Return validation split."
+        """Return validation split.
+
+        If the split is not yet loaded, load it. Returns an Nx3 IntTensor of
+        spo-triples.
+
+        """
         return self.split("valid")
 
     def test(self) -> Tensor:
-        "Return test split."
+        """Return test split.
+
+        If the split is not yet loaded, load it. Returns an Nx3 IntTensor of
+        spo-triples.
+
+        """
         return self.split("test")
 
     def entity_ids(
         self, indexes: Optional[Union[int, Tensor]] = None
     ) -> Union[str, List[str], np.ndarray]:
-        """Decode entity ids.
+        """Decode indexes to entity ids.
 
-        Shortcut for `self.map_indexes(indexes, "entity_ids")`.
+        See `Dataset#map_indexes` for a description of the `indexes` argument.
         """
         return self.map_indexes(indexes, "entity_ids")
 
     def relation_ids(
         self, indexes: Optional[Union[int, Tensor]] = None
     ) -> Union[str, List[str], np.ndarray]:
-        """Decode relation ids.
+        """Decode indexes to relation ids.
 
-        Shortcut for `self.map_indexes(indexes, "relation_ids")`.
+        See `Dataset#map_indexes` for a description of the `indexes` argument.
         """
         return self.map_indexes(indexes, "relation_ids")
+
+    def entity_strings(
+        self, indexes: Optional[Union[int, Tensor]] = None
+    ) -> Union[str, List[str], np.ndarray]:
+        """Decode indexes to entity strings.
+
+        See `Dataset#map_indexes` for a description of the `indexes` argument.
+
+        """
+        map_ = self.load_map(
+            "entity_strings", as_list=True, ids_key="entity_ids", ignore_duplicates=True
+        )
+        return self._map_indexes(indexes, map_)
+
+    def relation_strings(
+        self, indexes: Optional[Union[int, Tensor]] = None
+    ) -> Union[str, List[str], np.ndarray]:
+        """Decode indexes to relation strings.
+
+        See `Dataset#map_indexes` for a description of the `indexes` argument.
+
+        """
+        map_ = self.load_map(
+            "relation_strings",
+            as_list=True,
+            ids_key="relation_ids",
+            ignore_duplicates=True,
+        )
+        return self._map_indexes(indexes, map_)
 
     def meta(self, key: str) -> Any:
         """Return metadata stored under the specified key."""
@@ -227,6 +326,9 @@ class Dataset(Configurable):
 
         If the index has not yet been computed, computes it by calling the function
         specified in `self.index_functions`.
+
+        See `kge.indexing.create_default_index_functions()` for the indexes available by
+        default.
 
         """
         if key not in self._indexes:
@@ -249,7 +351,7 @@ class Dataset(Configurable):
     def map_indexes(
         self, indexes: Optional[Union[int, Tensor]], key: str
     ) -> Union[Any, List[Any], np.ndarray]:
-        """Maps indexes to values using the specified key.
+        """Maps indexes to values using the specified map.
 
         `key` refers to the key of a map file of the dataset, which associates a value
         with each numerical index. The map file is loaded automatically.
