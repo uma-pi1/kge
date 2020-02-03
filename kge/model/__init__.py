90ecaba90500c201328116501500e8dc14052608
samuelbroscheit <samuel.broscheit@gmail.com>
2020-02-04 00:49:13 +0100

    fix init wrt tucker3

--- kge/model/__init__.py
+++ kge/model/__init__.py
@@ -3,13 +3,13 @@ from kge.model.kge_model import KgeModel, KgeEmbedder
 # embedders
 from kge.model.embedder.lookup_embedder import LookupEmbedder
 from kge.model.embedder.projection_embedder import ProjectionEmbedder
-from kge.model.tucker3 import Tucker3RelationEmbedder
+from kge.model.embedder.tucker3_relation_embedder import Tucker3RelationEmbedder
 
 # models
 from kge.model.complex import ComplEx
 from kge.model.conve import ConvE
 from kge.model.distmult import DistMult
-from kge.model.tucker3 import RelationalTucker3
+from kge.model.relational_tucker3 import RelationalTucker3
 from kge.model.rescal import Rescal
 from kge.model.transe import TransE
 
