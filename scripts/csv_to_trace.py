c8ec7e55d6194e27abdf3943358e340809347cb5
Rainer Gemulla <rgemulla@uni-mannheim.de>
2020-02-25 17:57:14 +0100

    Clean-up old scripts and examples

--- scripts/csv_to_trace.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-import pandas as pd
-import yaml
-
-df = pd.read_csv(sys.stdin)
-df.apply(lambda row: print(yaml.dump(row.to_dict(), width=float("inf"), default_flow_style=True).strip()), axis=1)
