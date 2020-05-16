uma-pi1/kge

commit 9f9e12fe67d146638d7763dd3513dedf0719f39a

Seed numpy at each worker

kge/job/train.py
@@ -20,6 +20,10 @@ import kge.job.util
 SLOTS = [0, 1, 2]
 S, P, O = SLOTS
 
+def _worker_init_fn(worker_num):
+    # ensure that NumPy uses different seeds at each worker
+    np.random.seed()
+
 
 class TrainingJob(Job):
     """Abstract base job to train a single model with a fixed set of hyperparameters.
@@ -590,6 +594,7 @@ class TrainingJobKvsAll(TrainingJob):
             shuffle=True,
             batch_size=self.batch_size,
             num_workers=self.config.get("train.num_workers"),
+            worker_init_fn=_worker_init_fn,
             pin_memory=self.config.get("train.pin_memory"),
         )
 
@@ -797,6 +802,7 @@ class TrainingJobNegativeSampling(TrainingJob):
             shuffle=True,
             batch_size=self.batch_size,
             num_workers=self.config.get("train.num_workers"),
+            worker_init_fn=_worker_init_fn,
             pin_memory=self.config.get("train.pin_memory"),
         )
 
@@ -1031,6 +1037,7 @@ class TrainingJob1vsAll(TrainingJob):
             shuffle=True,
             batch_size=self.batch_size,
             num_workers=self.config.get("train.num_workers"),
+            worker_init_fn=_worker_init_fn,
             pin_memory=self.config.get("train.pin_memory"),
         )
 

commit bdb078f62560b9ebb4be568358e5d04b8d5d7f33

Only do numpy seeding if numpy seed is not fixed, fix regression introduced by 9f9e12fe67d146638d7763dd3513dedf0719f39a

kge/job/train.py
@@ -21,6 +21,7 @@ import kge.job.util
 SLOTS = [0, 1, 2]
 S, P, O = SLOTS
 
+
 def _worker_init_fn(worker_num):
     # ensure that NumPy uses different seeds at each worker
     np.random.seed()
@@ -595,7 +596,9 @@ class TrainingJobKvsAll(TrainingJob):
             shuffle=True,
             batch_size=self.batch_size,
             num_workers=self.config.get("train.num_workers"),
-            worker_init_fn=_worker_init_fn,
+            worker_init_fn=_worker_init_fn
+            if self.config.get("random_seed.numpy") == -1
+            else None,
             pin_memory=self.config.get("train.pin_memory"),
         )
 
@@ -803,7 +806,9 @@ class TrainingJobNegativeSampling(TrainingJob):
             shuffle=True,
             batch_size=self.batch_size,
             num_workers=self.config.get("train.num_workers"),
-            worker_init_fn=_worker_init_fn,
+            worker_init_fn=_worker_init_fn
+            if self.config.get("random_seed.numpy") == -1
+            else None,
             pin_memory=self.config.get("train.pin_memory"),
         )
 
@@ -1038,7 +1043,9 @@ class TrainingJob1vsAll(TrainingJob):
             shuffle=True,
             batch_size=self.batch_size,
             num_workers=self.config.get("train.num_workers"),
-            worker_init_fn=_worker_init_fn,
+            worker_init_fn=_worker_init_fn
+            if self.config.get("random_seed.numpy") == -1
+            else None,
             pin_memory=self.config.get("train.pin_memory"),
         )
 