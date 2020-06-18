618e1b26e88fc4d42eea0f3f341c93fb727186e2
Tara Safavi <tsafavi@umich.edu>
2020-06-18 13:01:58 +0000

    Factor out embedding initialization for use in KgeEmbedder subclasses

--- kge/model/embedder/projection_embedder.py
+++ kge/model/embedder/projection_embedder.py
@@ -25,18 +25,7 @@ class ProjectionEmbedder(KgeEmbedder):
         self.dropout = self.get_option("dropout")
         self.regularize = self.check_option("regularize", ["", "lp"])
         self.projection = torch.nn.Linear(self.base_embedder.dim, self.dim, bias=False)
-
-        init_ = self.get_option("initialize")
-        try:
-            init_args = self.get_option("initialize_args." + init_)
-        except KeyError:
-            init_args = self.get_option("initialize_args")
-            
-        self.initialize(
-            self.projection.weight.data,
-            init_,
-            init_args
-        )
+        self._init_embeddings(self.projection.weight.data)
 
     def _embed(self, embeddings):
         embeddings = self.projection(embeddings)
