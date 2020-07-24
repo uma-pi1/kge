bd57d295d0219b66b72de5968bedd75ba14a6e62
nzteb <patrickmbetz@gmail.com>
2020-07-24 09:33:37 +0200

    Add RawSplits in preprocessing

--- data/preprocess/util.py
+++ data/preprocess/util.py
@@ -1,8 +1,44 @@
 import numpy as np
 from os import path
-from dataclasses import dataclass
-from typing import Any, Callable, Dict, List, Optional, Union
+from dataclasses import dataclass, field
+from typing import Any, Callable, Dict, List, Optional, Union, Iterable
 import yaml
+
+
+@dataclass
+class RawSplit:
+    """A raw split.
+
+    Attributes:
+        name (str): Name of the raw split
+        file (str): File with tab-separated raw triples (can be labeled)
+        raw_data (list): List with triples encoded by raw id's
+        size (str): Number of triples.
+        entities (dict): Entities contained in this split. Keys refer to raw
+            id's and values to a dense index assigned during pre-processing.
+        relations (dict): See entities.
+        create_filtered (bool): If true, an additional filtered split will be created.
+        filter_with (RawSplit): Dictionary mapping with entities that should be included.
+        meta: (dict): Additional entries that this split adds to its config entries.
+
+    """
+    # fields defined by user
+    name: str
+    file: str
+    create_filtered: bool
+    filter_with = None
+    create_sample: bool
+    sample_size: int = None
+    meta: Dict = None
+
+    # fields defined during pre-processing
+    raw_data: List[str] = None
+    size: int = None
+    entities: Dict = field(default_factory=dict)
+    relations: Dict = field(default_factory=dict)
+    filtered_size: int = None
+    create_sample: bool = False
+
 
 
 @dataclass
@@ -13,48 +49,36 @@ class RawDataset:
      created automatically in analyze_raw_splits().
 
      Attributes:
-         raw_split_data (dict): Keys refer to raw splits and values are list of raw
-            triples (can be labeled) represented with entity/relations-ids.
-         raw_split_sizes (dict): Sizes of the raw splits.
+         raw_splits (list[RawSplit]): List of RawSplits.
          all_entities (dict): Distinct entities over all splits. Keys refer to
             raw entity-id's and values to the dense index.
          all_relations (dict): See all entities.
-         entities_in_split (dict[dict]): A key refers to a raw split and values are
-            dicts containing a mapping similar to all_entities for the respective split.
-         relations_in_split (dict): See entities_in_split.
          config (dict): Raw dictionary holding the dataset config options.
          folder (str): Path to the dataset folder.
 
      """
-    raw_split_data: Dict[str, List[List[str]]]
-    raw_split_sizes: Dict[str, int]
+    raw_splits: List[RawSplit]
     all_entities: Dict[str, int]
     all_relations: Dict[str, int]
-    entities_in_split: Dict[str, Dict[str, int]]
-    relations_in_split: Dict[str, Dict[str, int]]
     config: Dict[str, str]
     folder: str
+    order_sop: bool = False
 
 
 def analyze_raw_splits(
-        raw_split_files: dict,
+        raw_splits: List[RawSplit],
         folder: str,
-        collect_objects_in: List[str],
         order_sop: bool = False,
 
 ) -> RawDataset:
-    """Read a collection of raw split files and create metadata.
+    """Read a collection of raw splits and create a RawDataset.
 
     Args:
-        raw_split_files (dict): Keys refer to raw split names and values to filenames.
-            Files have to be tab-separated files of triples (can be labeled).
+        raw_splits (list[RawSplits]): List of RawSplits.
         folder (str): Folder of the dataset containing the files.
-        collect_objects_in (list[str]): For every raw split in the list, additionally
-            collect relations + entities existing in this split.
         order_sop (bool): True when the order in the raw triples is (S,O,P) instead of
             (S,P,O).     
     """
-
 
     if order_sop:
         S, P, O = 0, 2, 1
@@ -62,18 +86,14 @@ def analyze_raw_splits(
         S, P, O = 0, 1, 2
 
     # read data and collect entities and relations
-    split_sizes = {}
-    raw = {}
     all_entities = {}
     all_relations = {}
-    entities_in_split = {split: {} for split in collect_objects_in}
-    relations_in_split = {split: {} for split in collect_objects_in}
     ent_id = 0
     rel_id = 0
-    for split, filename in raw_split_files.items():
-        with open(folder + "/" + filename, "r") as f:
-            raw[split] = list(map(lambda s: s.strip().split("\t"), f.readlines()))
-            for t in raw[split]:
+    for split in raw_splits:
+        with open(path.join(folder, split.file), "r") as f:
+            split.raw_data = list(map(lambda s: s.strip().split("\t"), f.readlines()))
+            for t in split.raw_data:
                 if t[S] not in all_entities:
                     all_entities[t[S]] = ent_id
                     ent_id += 1
@@ -83,16 +103,17 @@ def analyze_raw_splits(
                 if t[O] not in all_entities:
                     all_entities[t[O]] = ent_id
                     ent_id += 1
-                if split in collect_objects_in:
-                    entities_in_split[split][t[S]] = all_entities[t[S]]
-                    entities_in_split[split][t[O]] = all_entities[t[O]]
-                    relations_in_split[split][t[P]] = all_relations[t[P]]
+                # TODO you could use a flag for a raw split if it is desired to save
+                #  its objects separately, e.g. only for the train split it is needed
+                #  for the current dataset
+                split.entities[t[S]] = all_entities[t[S]]
+                split.entities[t[O]] = all_entities[t[O]]
+                split.relations[t[P]] = all_relations[t[P]]
+            split.size = len(split.raw_data)
             print(
-                f"Found {len(raw[split])} triples in {split} split "
-                f"(file: {filename})."
+                f"Found {split.size} triples in {split.name} split "
+                f"(file: {split.file})."
             )
-            split_sizes[split] = len(raw[split])
-
     print(f"{len(all_relations)} distinct relations")
     print(f"{len(all_entities)} distinct entities")
 
@@ -101,14 +122,12 @@ def analyze_raw_splits(
     )
 
     dataset = RawDataset(
-            raw_split_data=raw,
-            raw_split_sizes=split_sizes,
+            raw_splits=raw_splits,
             all_entities=all_entities,
             all_relations=all_relations,
-            entities_in_split=entities_in_split,
-            relations_in_split=relations_in_split,
             config=config,
             folder=folder,
+            order_sop=order_sop,
     )
 
     # write entity/relation maps and update config
@@ -137,6 +156,108 @@ def store_map(symbol_map: dict, filename: str):
 def write_triple(f, ent, rel, t, S, P, O):
     """Write a triple to a file. """
     f.write(str(ent[t[S]]) + "\t" + str(rel[t[P]]) + "\t" + str(ent[t[O]]) + "\n")
+
+
+def write_split(
+        raw_triples: List[List],
+        file_name: str,
+        entities: dict,
+        relations: dict,
+        order_sop: bool = False,
+        create_sample: bool = False,
+        sample_size: int = None,
+        sample_file: str = None,
+        create_filtered: bool = False,
+        filtered_file: str = None,
+        filtered_include_ent: Union[dict, list] = None,
+        filtered_include_rel: Union[dict, list] = None,
+):
+    """From a raw split, write a split file using indexes.
+
+     Optionally, a filtered split file and a sample of the raw split can be created.
+
+     Args:
+         triples (list): List of triples.
+         file_name (str): File name where the split is written to.
+         entities (dict): mapping of all entities
+         relations (dict): mapping of all relations
+         order_sop (bool): True when the order of the triples in "dataset" is (S,O,P)
+         create_sample (bool): Write an additional random subset of the split.
+         sample_size (int): Size of the subset.
+         sample_file (str): Filename of the subset.
+         create_filtered(bool): Write an additional filtered version of the split.
+         filtered_file (str): Filename of the filtered split.
+         filtered_include_ent : Dict or List. Triples containing these entities will be
+         included in the filtered split.
+         filtered_include_rel: see filtered_include_ent.
+
+     """
+    if order_sop:
+        S, P, O = 0, 2, 1
+    else:
+        S, P, O = 0, 1, 2
+
+    # the sampled split is a randomly chosen subsample
+    if create_sample:
+        sample = np.random.choice(len(raw_triples), sample_size, False)
+        sample_f = open(path.join(sample_file), "w")
+
+    # the filtered split consists of triples from the raw split where both entities
+    # and relation exist in filtered_include_ent/filter_relation
+    if create_filtered:
+        filtered_size = 0
+        filter_f = open(filtered_file, "w")
+
+    with open(file_name, "w") as f:
+        for n, t in enumerate(raw_triples):
+            write_triple(f, entities, relations, t, S, P, O)
+            if create_sample and n in sample:
+                write_triple(sample_f, entities, relations, t, S, P, O)
+            if create_filtered and t[S] in filtered_include_ent \
+                    and t[O] in filtered_include_ent \
+                    and t[P] in filtered_include_rel:
+                write_triple(filter_f, entities, relations, t, S, P, O)
+                filtered_size += 1
+
+
+  # write to config everything you have done
+    dataset.config[f"files.{file_key}.size"] = dataset.raw_split_sizes[split]
+    if create_filtered:
+        dataset.config[f"files.{filtered_key}.size"] = filtered_size
+    if create_sample:
+        dataset.config[f"files.{sample_key}.size"] = sample_size
+
+
+def process_dataset_splits(raw_dataset: RawDataset):
+    for raw_split in raw_dataset.raw_splits:
+        write_split(
+            raw_triples=raw_split.raw_data,
+            file_name=path.join(raw_dataset.folder, f"{raw_split.file}.del"),
+            entitie=raw_dataset.all_entities,
+            relations=raw_dataset.all_relations,
+            order_sop=raw_dataset.order_sop,
+            create_sample=raw_split.create_sample,
+            sample_size=raw_split.sample_size,
+            sample_file=path.join(raw_dataset.folder, f"{raw_split.file}_sample.del"),
+            create_filtered=raw_split.create_filtered
+            filtered_file=
+
+        )
+
+
+ raw_triples: List[List],
+        file_name: str,
+        entities: dict,
+        relations: dict,
+        order_sop: bool = False,
+        create_sample: bool = False,
+        sample_size: int = None,
+        sample_file: str = None,
+        create_filtered: bool = False,
+        filtered_file: str = None,
+        filtered_include_ent: Union[dict, list] = None,
+        filtered_include_rel: Union[dict, list] = None,
+
 
 
 def process_split(
