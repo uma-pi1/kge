6df2a3dac9f271b59796591abbfe2e66d11ddac8
AdrianKs <adrian.kochsiek@outlook.de>
2020-09-10 09:28:22 +0200

    fix memory leak in append_num_parameter post epoch hook

--- kge/model/kge_model.py
+++ kge/model/kge_model.py
@@ -584,7 +584,7 @@ class KgeModel(KgeBase):
         if isinstance(job, TrainingOrEvaluationJob):
             def append_num_parameter(job):
                 job.current_trace["epoch"]["num_parameters"] = sum(
-                    map(lambda p: p.numel(), self.parameters())
+                    map(lambda p: p.numel(), job.model.parameters())
                 )
 
             job.post_epoch_hooks.append(append_num_parameter)
