d7a642843c1a6050fadcad00fb69656e142d4543
AdrianKs <adrian.kochsiek@outlook.de>
2020-10-22 17:26:46 +0200

    store checkpoint of initialization

--- kge/job/train.py
+++ kge/job/train.py
@@ -143,6 +143,8 @@ class TrainingJob(TrainingOrEvaluationJob):
             raise Exception(
                 f"{self.__class__.__name__} was initialized for forward only. You can only call run_epoch()"
             )
+        if self.epoch == 0:
+            self.save(self.config.checkpoint_file("init"))
 
         self.config.log("Starting training...")
         checkpoint_every = self.config.get("train.checkpoint.every")
