a83efa732afe4289b83ac7507b6eaec2081a065c
sailera19 <sailer.alex@googlemail.com>
2021-01-10 17:38:17 +0100

    adjusted olpbench_samuel.yaml to 8192 negative sample size and fixed bug in model init

--- kge/model/__init__.py
+++ kge/model/__init__.py
@@ -4,7 +4,6 @@ from kge.model.kge_model import KgeModel, KgeEmbedder
 from kge.model.embedder.lookup_embedder import LookupEmbedder
 from kge.model.embedder.projection_embedder import ProjectionEmbedder
 from kge.model.embedder.tucker3_relation_embedder import Tucker3RelationEmbedder
-from kge.model.embedder.unigram_pooling_embedder import UnigramPoolingEmbedder
 from kge.model.embedder.mention_embedder import MentionEmbedder
 from kge.model.embedder.unigram_lookup_embedder import UnigramLookupEmbedder
 from kge.model.embedder.bigram_lookup_embedder import BigramLookupEmbedder
