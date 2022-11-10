3ba5ef85c2d00e7a7e2679bbce07b7f921a28736
fniesel <57442202+fniesel@users.noreply.github.com>
2022-11-10 15:28:02 +0100

    Update version requirements (#39)
    * Updated the version requirements
    
    * Cleanup

--- setup.py
+++ setup.py
@@ -9,23 +9,21 @@ setup(
     author_email="rgemulla@uni-mannheim.de",
     packages=["kge"],
     install_requires=[
-        "numpy==1.19.*",
-        "torch==1.7.1",
+        "numpy>=1.19.*",
+        "torch>=1.7.1",
         "pyyaml",
         "pandas",
         "argparse",
         "path",
         # please check correct behaviour when updating ax platform version!!
-        "ax-platform==0.1.19", "botorch==0.4.0", "gpytorch==1.4.2",
+        "ax-platform>=0.1.19", "botorch>=0.4.0", "gpytorch>=1.4.2",
         "sqlalchemy",
         "torchviz",
         # LibKGE uses numba typed-dicts which is part of the experimental numba API
         # see http://numba.pydata.org/numba-doc/0.50.1/reference/pysupported.html
-        "numba==0.50.*",
+        "numba>=0.50.*",
     ],
-    # Ax 0.1.10 requires python 3.7. Numba does not yet support for Python 3.9:
-    # https://github.com/numba/numba/issues/6345
-    python_requires=">=3.7,<3.9",
+    python_requires=">=3.7",
     zip_safe=False,
     entry_points={"console_scripts": ["kge = kge.cli:main",],},
 )
